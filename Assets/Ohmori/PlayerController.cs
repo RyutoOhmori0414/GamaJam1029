63ae0ed3734d9cf485e86af364fd5e4dd3ff3f54
RyutoOhmori <ryutoomori0414@gmail.com>
2022-10-29 13:48:13 +0900

    [add] プレイヤーが隠れるようにした。

--- Assets/Ohmori/PlayerController.cs
+++ Assets/Ohmori/PlayerController.cs
@@ -9,37 +9,63 @@ public class PlayerController : MonoBehaviour
     float _moveSpeed = 10f;
     [SerializeField, Tooltip("プレイヤーのジャンプのパワー")]
     float _jumpSpeed = 10f;
-    /// <summary>プレイヤーが隠れているかどうか</summary>
+    /// <summary>プレイヤーが隠れているかどうかで移動を制限</summary>
     bool _isHiding = false;
+    /// <summary>プレイヤーが隠れているかどうか</summary>
+    public bool IsHiding { get { return _isHiding; } }
+    /// <summary>現在隠れられるのかどうか</summary>
+    bool _isHidable = false;
 
     Rigidbody2D _rb;
+    SpriteRenderer _sr;
+    Collider2D _collider;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _sr = GetComponent<SpriteRenderer>();
     }
     void Update()
     {
-
-        // ジャンプの処理
-        if (Input.GetButtonDown("Jump"))
+        // 隠れていないときは移動できる
+        if (!_isHiding)
         {
-            _rb.AddForce(Vector2.up *_jumpSpeed, ForceMode2D.Impulse);
+            // ジャンプの処理
+            if (Input.GetButtonDown("Jump"))
+            {
+                _rb.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            }
+            // 移動の処理
+            float h = Input.GetAxisRaw("Horizontal");
+
+            _rb.velocity = Vector3.right * h * _moveSpeed + Vector3.up * _rb.velocity.y;
+
+            if (Input.GetButtonDown("Hide") && _isHidable)
+            {
+                _isHiding = true;
+                // 隠れた瞬間に行われる処理を書く
+                _sr.color = Color.clear; // 仮に透明にする、半透明が完成形
+            }
         }
-        // 移動の処理
-        float h = Input.GetAxisRaw("Horizontal");
-
-        _rb.velocity = Vector3.right * h + Vector3.up * _rb.velocity.y;
-
         //現れる処理
+        else if (Input.GetButtonDown("Hide"))
+        {
+            _isHiding = false;
+            _sr.color = Color.yellow;
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         // 隠れる処理
-        if (collision.gameObject.CompareTag("HideObject") && Input.GetButtonDown("Hide"))
+        if (collision.gameObject.CompareTag("HideObject"))
         {
-            _isHiding = true;
+            _isHidable = true;
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _isHidable = false;
     }
 }
