ad194f2c9d0c1feb581763a06788de67390cd0d3
伊藤廣軒 <106290558+ItouHiroken@users.noreply.github.com>
2022-10-29 16:51:37 +0900

    [Add]攻撃の効果音を追加した

--- Assets/Itou/Sozaiyasan/SozaiMoto.cs
+++ Assets/Itou/Sozaiyasan/SozaiMoto.cs
@@ -2,4 +2,5 @@ public class SozaiMoto
 {
     //Font https://ux.getuploader.com/sumineko/download/1/Ounen-mouhitsu.otf
     //BGM https://pocket-se.info/
+    //SE https://dova-s.jp/se/play1167.html
 }
